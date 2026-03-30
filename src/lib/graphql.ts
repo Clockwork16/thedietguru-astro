55fdfc4ccc9188ed99511d779e4ac725b1690995
Clockwork16 <146893289+Clockwork16@users.noreply.github.com>
2026-03-30 18:31:41 -0400

    Update GraphQL URL to include trailing slash

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,4 +1,4 @@
-const WP_GRAPHQL_URL = 'https://thedietguru.org/backend/graphql';
+const WP_GRAPHQL_URL = 'https://thedietguru.org/backend/graphql/';
 
 export interface GraphQLResponse<T = any> {
   data: T;
