c49d618e5751bd851d781fed805e3adf837eeec9
Clockwork16 <clockwork16@github.com>
2026-07-25 10:12:42 -0400

    Add @astrojs/sitemap integration and public/robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    site: 'https://thedietguru.org',
+    integrations: [
+        tailwind(),
+        sitemap()
+    ],
 });
